02b4bdf064b737f77e930f67495b6512823584fe
qyf381389362 <381389362@qq.com>
2018-03-03 19:12:34 +0800

    修改路由

--- src/router/router.js
+++ src/router/router.js
@@ -2,13 +2,13 @@
  * @Author: qinyufei
  * @Date: 2018-03-02 16:33:13
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-03-03 17:09:23
+ * @Last Modified time: 2018-03-03 17:32:48
  */
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/components/HomePage'
-import DevelopmentPlan from '../components/Plan/SoftDevelopmentPlan'
-import VerificationPlan from '../components/Plan/SoftwareVerificationPlan'
+import HomePage from '@/views/HomePage'
+import DevelopmentPlan from '../views/Plan/SoftDevelopmentPlan'
+import VerificationPlan from '../views/Plan/SoftwareVerificationPlan'
 
 Vue.use(Router)
 
