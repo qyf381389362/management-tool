b94b5b1e59b5f92c6ef9c85642726ade05f81a57
qyf381389362 <381389362@qq.com>
2018-07-05 00:06:57 +0800

    增加了作者信息

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -1,3 +1,10 @@
+/*
+ * @Author: 秦雨霏 
+ * @Date: 2018-07-04 23:21:35 
+ * @Last Modified by:   秦雨霏 
+ * @Last Modified time: 2018-07-04 23:21:35 
+ */
+
 <template>
   <div>
     <div ref="toolbar" class="toolbar"></div>
